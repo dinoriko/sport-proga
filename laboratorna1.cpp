e85d0daa51dac43ce2843fe71cbdc2ab486ee11e
dino <i.am.romanovna@gmail.com>
2025-06-04 03:14:18 +0300

    Initial commit: added code and report

---
+++ laboratorna1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float result = 5 / 2.0;
+    cout << "Result of 5 / 2: " << result << endl;
+
+    const float pi = 3.14;
+    cout << "Constant pi: " << pi << endl;
+
+    char ch = 'A';
+    cout << "ASCII code of char: " << (int)ch << endl;
+
+    int i;
+    float f;
+    cout << "Enter an integer: ";
+    cin >> i;
+    cout << "Enter a float: ";
+    cin >> f;
+    cout << "Sum: " << i + f << endl;
+
+    char inputChar;
+    cout << "Enter a character: ";
+    cin >> inputChar;
+    char nextChar = inputChar + 1;
+    cout << "Next ASCII character: " << nextChar << endl;
+
+    return 0;
+}
